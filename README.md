509da2bd7c0ffc4726feccc1853118e6e1c6f052
Practice Golang <practice.golang@gmail.com>
2019-05-29 15:37:55 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/burke/howto-go-with-cpp
---
+++ foo.go
@@ -0,0 +1,27 @@
+package main // import "gofoo"
+
+// // #cgo LDFLAGS: -L./ -lfoo -lstdc++
+// #include "foo.h"
+import "C"
+
+type goFoo struct {
+	foo C.Foo
+}
+
+func new() goFoo {
+	var ret goFoo
+	ret.foo = C.FooInit()
+	return ret
+}
+func (f goFoo) Free() {
+	C.FooFree(f.foo)
+}
+func (f goFoo) Bar() {
+	C.FooBar(f.foo)
+}
+
+func main() {
+	foo := new()
+	foo.Bar()
+	foo.Free()
+}
